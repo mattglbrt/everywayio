c514717ee98a3a8cc0a4c115fd14006efb8505df
mattglbrt <matt@everyway.io>
2025-03-04 15:05:43 -0500

    First Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,50 @@
+import { type Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
+import clsx from 'clsx'
+
+import { Providers } from '@/app/providers'
+
+import '@/styles/tailwind.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+const monaSans = localFont({
+  src: '../fonts/Mona-Sans.var.woff2',
+  display: 'swap',
+  variable: '--font-mona-sans',
+  weight: '200 900',
+})
+
+export const metadata: Metadata = {
+  title: 'Everyway - Work Less. Accomplish More.',
+  description:
+    'Unlock the full potential of AI and enhance your customer experience with our expert consulting services. Sign up today to be the first to know when we begin accepting clients, and take the next step towards transforming your business.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang="en"
+      className={clsx('h-full antialiased', inter.variable, monaSans.variable)}
+      suppressHydrationWarning
+    >
+      <body className="flex min-h-full flex-col bg-white dark:bg-gray-950">
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
---
+++ src/components/Intro.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link'
+
+import { IconLink } from '@/components/IconLink'
+import { Logo } from '@/components/Logo'
+import { SignUpForm } from '@/components/SignUpForm'
+
+function BookIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg viewBox="0 0 16 16" aria-hidden="true" fill="currentColor" {...props}>
+      <path d="M7 3.41a1 1 0 0 0-.668-.943L2.275 1.039a.987.987 0 0 0-.877.166c-.25.192-.398.493-.398.812V12.2c0 .454.296.853.725.977l3.948 1.365A1 1 0 0 0 7 13.596V3.41ZM9 13.596a1 1 0 0 0 1.327.946l3.948-1.365c.429-.124.725-.523.725-.977V2.017c0-.32-.147-.62-.398-.812a.987.987 0 0 0-.877-.166L9.668 2.467A1 1 0 0 0 9 3.41v10.186Z" />
+    </svg>
+  )
+}
+
+
+function XIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg viewBox="0 0 16 16" aria-hidden="true" fill="currentColor" {...props}>
+      <path d="M9.51762 6.77491L15.3459 0H13.9648L8.90409 5.88256L4.86212 0H0.200195L6.31244 8.89547L0.200195 16H1.58139L6.92562 9.78782L11.1942 16H15.8562L9.51728 6.77491H9.51762ZM7.62588 8.97384L7.00658 8.08805L2.07905 1.03974H4.20049L8.17706 6.72795L8.79636 7.61374L13.9654 15.0075H11.844L7.62588 8.97418V8.97384Z" />
+    </svg>
+  )
+}
+
+export function Intro() {
+  return (
+    <>
+      <div>
+        <Link href="/">
+          <Logo className="inline-block h-8 w-auto" />
+        </Link>
+      </div>
+      <h1 className="mt-14 font-display text-4xl/tight font-light text-white">
+        Work Less.<br />
+        <span className="text-sky-300">Accomplish More.</span>
+      </h1>
+      <p className="mt-4 text-sm/6 text-gray-300">
+      Unlock the full potential of AI and enhance your customer experience with our expert consulting services. Sign up today to be the first to know when we begin accepting clients, and take the next step towards transforming your business.
+      </p>
+      <SignUpForm />
+      <div className="mt-8 flex flex-wrap justify-center gap-x-1 gap-y-3 sm:gap-x-2 lg:justify-start">
+        <IconLink href="https://everywaydesign.com/about/" icon={BookIcon} className="flex-none">
+          About
+        </IconLink>
+        
+      </div>
+    </>
+  )
+}
+
+export function IntroFooter() {
+  return (
+    <p className="flex items-baseline gap-x-2 text-[0.8125rem]/6 text-gray-500">
+      Brought to you by{' '}
+      <a href="https://mattglbrt.com">Matt Gilbert</a>
+    </p>
+  )
+}
